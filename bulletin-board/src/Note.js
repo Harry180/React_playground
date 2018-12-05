a301fef01224a4f0313391a2bb24053d6f8bfc3e
Lukasz Charubin <lukasz.charubin@gmail.com>
2018-12-05 21:13:03 +0100

    Create board and change way of displaying notes on board.

--- bulletin-board/src/Note.js
+++ bulletin-board/src/Note.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
-import {FaPenFancy, FaTrash} from 'react-icons/fa'
+import {FaPenFancy, FaTrash, FaRecycle} from 'react-icons/fa'
 
 class Note extends Component {
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {
+            editing: false
+        };
         this.edit = this.edit.bind(this);
         this.remove = this.remove.bind(this);
+        this.renderForm = this.renderForm.bind(this);
+        this.renderDisplay = this.renderDisplay.bind(this);
+        this.save = this.save.bind(this);
     }
     edit() {
-        alert('edit')
+        this.setState({
+            editing:true
+        })
     }
     remove() {
-        alert('remove')
+        console.log('remove');
     }
-    render() {
+    save () {
+        console.log(this._newText.value);
+    }
+    renderForm(){
         return (
             <div className="note">
-                <p>Learn React</p>
+                <form>
+                    <textarea ref={input => this._newText = input}/>
+                    <button onClick={this.save}><FaRecycle /></button>
+                </form>
+            </div>
+        )
+    }
+    renderDisplay() {
+        return (
+            <div className="note">
+                <p>{this.props.children}</p>
                 <span>
                     <button onClick={this.edit} id="edit"><FaPenFancy /></button>
                     <button onClick={this.remove} id="remove"><FaTrash /></button>
                 </span>
             </div>
         )
+    }
+    render() {
+        if(this.state.editing){
+            return this.renderForm();
+        } else{
+            return this.renderDisplay();
+        }
     }
 }
 
